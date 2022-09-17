56dbe076dd11b19dd3568b9ea3593b1aeb02e7a8
Victor Galhardo Vela <35960632+VictorVela@users.noreply.github.com>
2022-09-17 14:01:36 -0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+    <h1>test</h1>
+  </React.StrictMode>
+)
